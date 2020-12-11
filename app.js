966287c64d5ffe3c62708fb38611d7b257c59b04
1styrGroupB <1styrGroupB@pn.local>
2020-12-11 10:38:45 +0800

    public

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
-
 const app = express()
+const http = require('http')
 const PORT = process.env.PORT || 5000
+const path = require('path')
+const socket = require('socket.io')
+
+const server = http.createServer()
+const io = socket(server)
 
 
+app.use(express.static(path.join(__dirname,'/public')))
 
 
-app.listen(PORT, () => {
+// app.get('/', (req, res) =>{
+//     res.render("index")
+// })
+ io.on('connection', socket => {
+     console.log("new connection")
+ })
+
+server.listen(PORT, () => {
     console.log( `Listening to PORT ${PORT}`)
 })
